a2af3125e19b999d6d5f2e0766502ca40259fb8f
ndesh02 <54901274+ndesh02@users.noreply.github.com>
2021-01-07 14:49:59 -0500

    Bird moves in a parabolic fashion and menu scene has been partially created

--- Player.cs
+++ Player.cs
@@ -3,19 +3,40 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export] public int speed = 200;
+    
+    public Vector2 velocity = new Vector2();
+    
+    //chose arbitrary temporary values for the following:
+    int ceiling=50;
+    int gravity=10;
+    int upward_vel=20;
+    int max_downward_vel=100;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    
+    public void GetInput()
     {
+        velocity.x+=(float)(0.1);
+        velocity.y+=5;
         
+        if (Input.IsActionPressed("right"))
+            velocity.x += 1;
+
+        if (Input.IsActionPressed("left"))
+            velocity.x -= 1;
+
+        if (Input.IsActionPressed("down"))
+            velocity.y += 1;
+
+        if (Input.IsActionJustPressed("up"))
+            velocity.y-=200;
+
+        velocity = velocity.Normalized() * speed;
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _PhysicsProcess(float delta)
+    {
+        GetInput();
+        velocity = MoveAndSlide(velocity);
+    }
 }
