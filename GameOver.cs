b41a5534cee085b7200898a590ebc6eddd197c13
Maadhava Ravishankar <m2ravish@uwaterloo.ca>
2021-01-11 13:54:08 -0500

    created game over screen

--- GameOver.cs
+++ GameOver.cs
@@ -10,7 +10,12 @@ public class GameOver : Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        GetNode("RestartButton").Connect("pressed", this, nameof(_OnRestartPressed));
+    }
+
+    public void _OnRestartPressed()
+    {
+        GetTree().ChangeScene("res://MainScene.tscn");
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
